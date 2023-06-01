5515ac6924ed93b56bce916d8ae1df6300ba8250
Ulrich Bittner <ubittner@users.noreply.github.com>
2023-06-01 11:49:37 +0200

    Update control.php

--- Aufwachmusik/helper/control.php
+++ Aufwachmusik/helper/control.php
@@ -80,10 +80,12 @@ trait Control
                 @RequestAction($this->ReadPropertyInteger('DeviceVolume'), 1);
             }
             //Set device preset
+            $powerOn = true;
             if ($this->CheckDevicePresetsID()) {
                 $devicePresetID = $this->ReadPropertyInteger('DevicePresets');
                 $preset = $this->GetValue('Presets');
-                if ($preset >= 1) {
+                if ($preset > 0) {
+                    $powerOn = false;
                     $setDevicePreset = @RequestAction($devicePresetID, $preset);
                     $this->SendDebug(__FUNCTION__, 'Preset: ' . $preset, 0);
                     //Try again
@@ -93,11 +95,13 @@ trait Control
                 }
             }
             //Power device on
-            $this->SendDebug(__FUNCTION__, 'Gerät einschalten', 0);
-            $powerOnDevice = @RequestAction($this->ReadPropertyInteger('DevicePower'), true);
-            //Try again
-            if (!$powerOnDevice) {
-                @RequestAction($this->ReadPropertyInteger('DevicePower'), true);
+            if ($powerOn) {
+                $this->SendDebug(__FUNCTION__, 'Gerät einschalten', 0);
+                $powerOnDevice = @RequestAction($this->ReadPropertyInteger('DevicePower'), true);
+                //Try again
+                if (!$powerOnDevice) {
+                    @RequestAction($this->ReadPropertyInteger('DevicePower'), true);
+                }
             }
             //Set next cycle
             $this->SetTimerInterval('IncreaseVolume', $this->CalculateNextCycle() * 1000);
